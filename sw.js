ae5da8e4f6a74bea97f9510ea63268b8188cafc4
xurxo77 <xurxoac@gmail.com>
2026-03-07 09:56:17 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,34 @@
 const CACHE_NAME = 'galicia-v1';
+
+// Solo cachear lo esencial
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/xurxo.jpg',
-  '/raquel.jpg',
-  '/Santiago.jpg',
-  '/ciesyons.jpg',
-  '/finisterre.jpg',
-  '/muralla.jpg',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png',
-  '/icons/icon-180.png'
+  './',
+  './index.html',
+  './manifest.json'
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .catch(err => console.log('Cache error:', err))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  e.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request)
+      .then(r => r || fetch(e.request))
+      .catch(() => caches.match('./index.html'))
+  );
 });
