fc18891de6cd32db107c5604f92148e7c80c2075
xurxo77 <xurxoac@gmail.com>
2026-03-14 08:46:10 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -39,3 +39,45 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+const CACHE_NAME = 'galicia-guia-v2'; 
+const urlsToCache = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.json'
+];
+
+// Instalación: forzamos a que se instale sin esperar
+self.addEventListener('install', event => {
+  self.skipWaiting(); 
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Activación: borramos cualquier caché que no sea la v2
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Borrando caché antigua:', cacheName);
+            return caches.delete(cacheName); 
+          }
+        })
+      );
+    }).then(() => self.clients.claim()) // Toma el control de la app al momento
+  );
+});
+
+// Interceptar peticiones: Siempre busca en red primero (Network First)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
